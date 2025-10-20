7e71af11f30a6ac18f7086b67cf13dca4e9518c7
Ritesh Chawla <riteshchawla@gmail.com>
2025-10-20 12:55:59 -0400

    Add minimum purchase filter to GetTopCustomers

--- database/GetTopCustomers.sql
+++ database/GetTopCustomers.sql
@@ -1,10 +1,13 @@
-CREATE PROCEDURE dbo.GetTopCustomers @TopN INT = 10
+CREATE PROCEDURE dbo.GetTopCustomers 
+	@TopN INT = 10,
+	@MinPurchases DECIMAL(10,2) = 0
 AS 
 BEGIN
 	SELECT TOP (@TopN)
-		CustomerId, 
+		CustomerId,
 		CustomerName,
 		TotalPurchases
 	FROM Customers
+	WHERE TotalPurchases >= @MinPurchases
 	ORDER BY TotalPurchases DESC;
 END
