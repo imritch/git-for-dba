e44449ffdb216b90f8fae84f4e1f9b038facd198
Ritesh Chawla <riteshchawla@gmail.com>
2025-10-20 12:05:38 -0400

    Add UpdateCustomer stored procedure

---
+++ database/UpdateCustomer.sql
@@ -0,0 +1,13 @@
+-- Update Customer Information 
+CREATE PROCEDURE dbo.UpdateCustomer
+	@CustomerId INT,
+	@CustomerName NVARCHAR(100),
+	@Email NVARCHAR(100)
+AS 
+BEGIN
+	UPDATE Customers
+	SET
+		CustomerName = @CustomerName,
+		Email = @Email
+	WHERE CustomerId = @CustomerId;
+END
