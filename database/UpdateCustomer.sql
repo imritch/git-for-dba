80bf3e18c80222ef3dcb2987bb1d28fcb3881201
Ritesh Chawla <riteshchawla@gmail.com>
2025-10-26 16:16:52 -0400

    Add phone number to UpdateCustomer SP

--- database/UpdateCustomer.sql
+++ database/UpdateCustomer.sql
@@ -2,12 +2,14 @@
 CREATE PROCEDURE dbo.UpdateCustomer
     @CustomerId INT,
     @CustomerName NVARCHAR(100),
-    @Email NVARCHAR(100)
+    @Email NVARCHAR(100),
+    @Phone NVARCHAR(20)
 AS
 BEGIN
     UPDATE Customers
     SET 
         CustomerName = @CustomerName,
-        Email = @Email
+        Email = @Email,
+        Phone = @Phone
     WHERE CustomerId = @CustomerId;
 END
