7df755b6efd29d588ffc39d1e5ae303edbe9455c
Ritesh Chawla <riteshchawla@gmail.com>
2025-10-26 16:43:33 -0400

    ***Merge Conflict Resolved*** Updated the sql statement to have both the date and minimum purchase criteria in the statement.

--- database/GetTopCustomers.sql
+++ database/GetTopCustomers.sql
@@ -1,6 +1,8 @@
 CREATE PROCEDURE dbo.GetTopCustomers
     @TopN INT = 10,
     @MinPurchases DECIMAL(10,2) = 0
+    @StartDate DATE = NULL
+
 AS
 BEGIN
     SELECT TOP (@TopN)
@@ -9,5 +11,6 @@ BEGIN
         TotalPurchases
     FROM Customers
     WHERE TotalPurchases >= @MinPurchases
+    AND @StartDate IS NULL OR RegistrationDate >= @StartDate
     ORDER BY TotalPurchases DESC;
 END
