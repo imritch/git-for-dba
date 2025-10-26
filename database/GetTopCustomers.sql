407beaaa16367ed4b14bc331e7875489c9e34d8e
Ritesh Chawla <riteshchawla@gmail.com>
2025-10-26 16:26:41 -0400

    Add GetTopCustomers SP

--- database/GetTopCustomers.sql
+++ database/GetTopCustomers.sql
@@ -1,15 +1,11 @@
-CREATE PROCEDURE dbo.GetTopCustomers 
-	@TopN INT = 10,
-	@MinPurchases DECIMAL(10,2) = 0,
-	@StartDate DATE = NULL
+CREATE PROCEDURE dbo.GetTopCustomers
+    @TopN INT = 10
 AS
 BEGIN
-	SELECT TOP (@TopN)
-		CustomerId,
-		CustomerName,
-		TotalPurchases
-	FROM Customers
-	WHERE TotalPurchases >= @MinPurchases
-	AND (@StartDate IS NULL OR RegistrationDate >= @StartDate) 
-	ORDER BY TotalPurchases DESC;
+    SELECT TOP (@TopN)
+        CustomerId,
+        CustomerName,
+        TotalPurchases
+    FROM Customers
+    ORDER BY TotalPurchases DESC;
 END
