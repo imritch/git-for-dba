d7df6d9b41e690468dcc8bc2d779aef759ee1c5c
Ritesh Chawla <riteshchawla@gmail.com>
2025-11-08 21:09:02 -0500

    Add GetCustomerOrders stored procedure

--- database/GetCustomerOrders.sql
+++ database/GetCustomerOrders.sql
@@ -1,24 +1,12 @@
--- Get Customer Orders with Error Handling
+-- Get Customer Orders
 CREATE PROCEDURE dbo.GetCustomerOrders
     @CustomerId INT
 AS
 BEGIN
-    SET NOCOUNT ON;
-    
-    BEGIN TRY
-        -- TODO: Add validation
-        -- TODO: Add logging
-        
-        SELECT 
-            OrderId,
-            OrderDate,
-            TotalAmount,
-            Status  -- Adding new column
-        FROM Orders
-        WHERE CustomerId = @CustomerId;
-        
-    END TRY
-    BEGIN CATCH
-        -- Error handling in progress...
-    END CATCH
+    SELECT 
+        OrderId,
+        OrderDate,
+        TotalAmount
+    FROM Orders
+    WHERE CustomerId = @CustomerId;
 END
